13a910cbbf967553618fa0d3f6068e4719de55d3
raizeta <rai_zet@hotmail.com>
2015-12-16 22:51:37 +0700

    Delete Directives

--- mobileluk/erp/angular/directives/directives.js
+++ mobileluk/erp/angular/directives/directives.js
@@ -1,18 +1,7 @@
 'use strict';
-myAppModule.directive('routeLoadingIndicator', function($rootScope) {
-  return {
-      restrict: 'E',
-      template: "<div class='col-lg-12' ng-if='isRouteLoading'><h1>Loading <i class='fa fa-cog fa-spin'></i></h1></div>",
-      link: function(scope, elem, attrs) {
-        scope.isRouteLoading = true;
+myAppModule.directive('navKepala', function($rootScope) {
+  return 
+  {
 
-        $rootScope.$on('$routeChangeStart', function() {
-          scope.isRouteLoading = true;
-        });
-
-        $rootScope.$on('$routeChangeSuccess', function() {
-          scope.isRouteLoading = true;
-        });
-      }
   };
 });
