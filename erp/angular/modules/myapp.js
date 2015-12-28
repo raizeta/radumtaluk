eca4cb4264b79bf2e5d54b332e121cd296155214
projectku <piter@lipat.co.id>
2015-12-28 15:12:14 +0700

    Commit From VS Code

--- erp/angular/modules/myapp.js
+++ erp/angular/modules/myapp.js
@@ -1,7 +1,7 @@
 'use strict';
 var myAppModule 	= angular.module('myAppModule',
 								['ngRoute','angularSpinner','ui.bootstrap','ngAnimate','oc.lazyLoad','signature',
-								 'ng-fusioncharts','naif.base64','monospaced.qrcode','ngCordova','ngMap']);
+								 'ng-fusioncharts','naif.base64','monospaced.qrcode','ngCordova','ngMap','mm.acl']);
 myAppModule.run(["$rootScope", "$location", function ($rootScope, $location) 
 {
     $rootScope.$on("$routeChangeSuccess", function (userInfo) 
